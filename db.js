c03621f55b40d66f75c30c469d7eaa69afaac020
Mahmoud <tkhattab1999@gmail.com>
2024-06-04 12:00:26 -0400

    Add files via upload

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'idea_processor',
-  password: 'Tamer123',
-  port: 5432,
-});
+  connectionString: "postgres://default:m2nK4zDCoewi@ep-lively-wood-a4euf753.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+})
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'idea_processor',
+//   password: 'Tamer123',
+//   port: 5432,
+// });
 
 module.exports = pool;
