7ad93886e394740bce14aefcd90305b80107cb5a
benhuangbmj <benjaminwong1985@gmail.com>
2024-11-30 16:20:37 -0500

    feat: create the function to get the event date and fix #9

---
+++ src/utils/dateTimeUtils.js
@@ -0,0 +1,34 @@
+const HTMLParser = require("node-html-parser");
+
+function getEventDate(text) {
+	const dateStr = extractDateStr(text);
+	const monthAndDay = extractMonthAndDay(dateStr);
+	if (monthAndDay === null) return new Date();
+	const year = analyseYear(monthAndDay);
+	return new Date(`${monthAndDay} ${year}`);
+
+	function extractDateStr(text) {
+		const description = HTMLParser.parse(text);
+		const l = description.childNodes.length;
+		const output = description.childNodes[l - 6].childNodes[0]._rawText;
+		return output;
+	}
+	function extractMonthAndDay(str) {
+		const regex = /(?<=, ).+(?= at)/;
+		if (regex.test(str)) {
+			const output = str.match(regex)[0];
+			return output;
+		} else {
+			return null;
+		}
+	}
+	function analyseYear(str) {
+		const currMonth = new Date(str).getMonth();
+		const nowMonth = new Date().getMonth();
+		const nowYear = new Date().getFullYear();
+		console.log(new Date(), nowYear, typeof nowYear);
+		return currMonth >= nowMonth ? nowYear : nowYear + 1;
+	}
+}
+
+module.exports = { getEventDate };
