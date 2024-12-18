f76512f7e743506d4de16aba5600516dfa5d77a2
benhuangbmj <benjaminwong1985@gmail.com>
2024-12-18 14:05:32 -0500

    chore: use url for database connection

--- src/tests/drizzle.config.ts
+++ src/tests/drizzle.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.test" });
 export default defineConfig({
     schema: "../db/schemas/**/*.ts",
     dialect: "postgresql",
     dbCredentials: {
-        host: "localhost",
-        port: 5433,
-        user: "postgres_test",
-        password: "mysecretpassword_test",
-        database: "mydatabase_test",
-        ssl: false,
+        url: process.env.DATABASE_URL!,
     },
 });
