663ea56f22e308bfc2e93a5afcd678d882f171ee
benhuangbmj <benjaminwong1985@gmail.com>
2025-01-04 11:27:53 -0500

    test: setup test database preseeding mechanics with sample data

--- src/tests/global-setup.js
+++ src/tests/global-setup.js
@@ -1,8 +1,11 @@
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, ".env.test") });
 const testPool = require(path.join(__dirname, "../../db"));
+const { drizzle } = require("drizzle-orm/node-postgres");
+const db = drizzle(testPool);
 
 module.exports = async () => {
-    globalThis.__POOL__ = testPool;
-    await require("./utils/handleDockerDb").setupDb(globalThis.__POOL__);
+  globalThis.__POOL__ = testPool;
+  await require("./utils/handleDockerDb").setupDb(globalThis.__POOL__);
+  await require("./utils/preseedDatabase")(db);
 };
