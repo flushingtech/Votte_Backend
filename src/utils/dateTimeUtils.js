121f62c96cb2174d7c28bfa9c98865c4bf9c2e63
benhuangbmj <benjaminwong1985@gmail.com>
2025-01-25 22:02:43 -0500

    fix: change the source of the date from RSS to the event page

--- src/utils/dateTimeUtils.js
+++ src/utils/dateTimeUtils.js
@@ -1,33 +1,43 @@
 const HTMLParser = require("node-html-parser");
+const fetch = require("node-fetch");
+const { JSDOM } = require("jsdom");
 
-function getEventDate(text) {
-	const dateStr = extractDateStr(text);
-	const monthAndDay = extractMonthAndDay(dateStr);
-	if (monthAndDay === null) return new Date();
-	const year = analyseYear(monthAndDay);
-	return new Date(`${monthAndDay} ${year}`);
+async function getEventDate(text) {
+    async function loadEventPageDOM() {
+        const url = text;
 
-	function extractDateStr(text) {
-		const description = HTMLParser.parse(text);
-		const l = description.childNodes.length;
-		const output = description.childNodes[l - 6].childNodes[0]._rawText;
-		return output;
-	}
-	function extractMonthAndDay(str) {
-		const regex = /(?<=, ).+(?= at)/;
-		if (regex.test(str)) {
-			const output = str.match(regex)[0];
-			return output;
-		} else {
-			return null;
-		}
-	}
-	function analyseYear(str) {
-		const currMonth = new Date(str).getMonth();
-		const nowMonth = new Date().getMonth();
-		const nowYear = new Date().getFullYear();
-		return currMonth >= nowMonth ? nowYear : nowYear + 1;
-	}
+        try {
+            const response = await fetch(url);
+            const html = await response.text();
+            const dom = new JSDOM(html);
+            const document = dom.window.document;
+            return document;
+        } catch (error) {
+            console.error("Error loading Meetup DOM:", error);
+        }
+    }
+    const evantPageDocument = await loadEventPageDOM();
+    const dateTime = evantPageDocument.querySelector("time").textContent;
+
+    const monthAndDay = extractMonthAndDay(dateTime);
+    if (monthAndDay === null) return new Date();
+    const year = analyseYear(monthAndDay);
+    return new Date(`${monthAndDay} ${year}`);
+    function extractMonthAndDay(str) {
+        const regex = /(?<=, ).+(?=,)/;
+        if (regex.test(str)) {
+            const output = str.match(regex)[0];
+            return output;
+        } else {
+            return null;
+        }
+    }
+    function analyseYear(str) {
+        const currMonth = new Date(str).getMonth();
+        const nowMonth = new Date().getMonth();
+        const nowYear = new Date().getFullYear();
+        return currMonth >= nowMonth ? nowYear : nowYear + 1;
+    }
 }
 
 module.exports = { getEventDate };
