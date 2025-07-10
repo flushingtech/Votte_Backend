522509c7b5cf524fb27addc13ad2f34905201c0c
Mahmoud <tkhattab1999@gmail.com>
2025-07-10 16:51:27 -0400

    fix(migrations): removed constraint query

--- src/db/migrations/0011_solid_bug.sql
+++ src/db/migrations/0011_solid_bug.sql
@@ -1,4 +1,2 @@
-ALTER TABLE "ideas" DROP CONSTRAINT "ideas_event_id_fkey";
---> statement-breakpoint
 ALTER TABLE "ideas" ALTER COLUMN "event_id" SET DATA TYPE text;--> statement-breakpoint
 ALTER TABLE "ideas" ALTER COLUMN "event_id" SET NOT NULL;
